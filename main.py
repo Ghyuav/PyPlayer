4e8aad8a05bd9d13828d1e874ad1c0a5b6d298e7
Ghyuav <89256344+Ghyuav@users.noreply.github.com>
2023-08-24 18:34:02 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from app import *
 from tkinter.ttk import *
 from ttkbootstrap import Style
-from tkinter import filedialog, StringVar, messagebox
+from tkinter import filedialog, StringVar, messagebox,Toplevel
 from math import floor
 from PIL import Image, ImageTk
 from os import startfile
 style = Style()
 window = style.master
 
-style.theme_use('darkly')
 window.title('Player')
 # window.iconbitmap(r'profile\ico.ico')
 # window.state('zoomed')
-window.geometry('1200x600')
+window.geometry('1200x650')
 
 
 length = 1
@@ -70,7 +69,8 @@ def playnext():
         load_file(playlist[playing_num])
     except IndexError as e:
         if str(e) == 'list index out of range':
-            pass # 列表播放完了
+            pass  # 列表播放完了
+
 
 def playlast():
     global playing_num, playlist, play_progress
@@ -80,7 +80,8 @@ def playlast():
         load_file(playlist[playing_num])
     except IndexError as e:
         if str(e) == 'list index out of range':
-            pass # 列表播放完了
+            pass  # 列表播放完了
+
 
 def open_file():
     global length, play_progress, playlist
@@ -89,18 +90,13 @@ def open_file():
 
 
 def load_file(path=''):
-    global length, play_progress, photo, pos_Scale,time_Label
+    global length, play_progress, photo, pos_Scale, time_Label
 
     try:
         data = player_load(path)
     except:
         messagebox.showerror('标题', '不支持的类型:'+path.split('.')[-1]+',切换到下一首')
-    try: # 若data不存在则不支持播放，直接跳过
-        if data:  # 不知什么原因可能返回为None
-            pass
-        else:
-            data = ['', '', '', '', '']
-
+    try:  # 若播放不了则不支持播放，直接跳过（有点草率，之后再来优化）
         if data[0]:
             title_str.set(data[0])
             window.title(f'{data[0]} - PyPlayer')
@@ -118,19 +114,19 @@ def load_file(path=''):
             img = Image.open('none.png').resize((512, 512))
         photo = ImageTk.PhotoImage(img)
         img_Label.configure(image=photo)
-        if data[4]: # 获取不了时长就禁用控件
+        if data[4]:  # 获取不了时长就禁用控件
             length = data[4]  # 歌曲总长度
-            pos_Scale['state']='normal'
-            time_Label['state']='normal'
+            pos_Scale['state'] = 'normal'
+            time_Label['state'] = 'normal'
         else:
             pos_Scale.set(0)
             time_str.set('∞')
-            pos_Scale['state']='disable'
-            time_Label['state']='disable'
+            pos_Scale['state'] = 'disable'
+            time_Label['state'] = 'disable'
             length = 0
         play_progress = 0  # 播放进度归零
         play()
-    
+
     except:
         playnext()
 
@@ -171,6 +167,22 @@ def pause():
         else:
             play()
 
+def change_theme():
+    global settings
+    style.theme_use(theme_name.get())
+    settings['theme'] = theme_name.get()
+    with open('settings.json','w') as f:
+        f.write(str(settings))
+def setting():
+    global theme_Combobox,theme_name
+    setting_window = Toplevel()
+    setting_window.geometry('400x200')
+    theme_name = StringVar()
+    Label(setting_window,text='选择一个主题: ').pack(side='left')
+    theme_Combobox = Combobox(setting_window,state="readonly",textvariable=theme_name,values=style.theme_names())
+    theme_name.set(settings["theme"])
+    theme_Combobox.pack(side='left',fill='x')
+    theme_Combobox.bind('<<ComboboxSelected>>',lambda a:change_theme())
 
 title_str = StringVar()
 artist_str = StringVar()
@@ -183,36 +195,39 @@ buttons_Frame = Frame(window)
 img = Image.open('none.png')
 photo = ImageTk.PhotoImage(img)
 img_Label = Label(window, image=photo)
-pos_Scale = Scale(from_=0, to=100, bootstyle="info")
+pos_Scale = Scale(from_=0, to=100, bootstyle="primary")
 title_Label = Label(window, textvariable=title_str, font=('微软雅黑', 25))
 artist_Label = Label(info_Frame, textvariable=artist_str, font=('微软雅黑', 15))
 album_Label = Label(info_Frame, textvariable=album_str, font=('微软雅黑', 15))
 ask_file_Button = Button(buttons_Frame, text="打开文件",
-                         bootstyle=('info', 'outline'), command=open_file)
+                         bootstyle=('primary', 'outline'), command=open_file)
+setting_Button = Button(buttons_Frame, text="选项",
+                         bootstyle=('primary', 'outline'), command=setting)
 pause_Button = Button(buttons_Frame, text='⏯️',
-                      bootstyle=('info', 'outline'), command=pause)
+                      bootstyle=('primary', 'outline'), command=pause)
 next_Button = Button(buttons_Frame, text='⏭️',
-                      bootstyle=('info', 'outline'), command=playnext)
+                     bootstyle=('primary', 'outline'), command=playnext)
 last_Button = Button(buttons_Frame, text='⏮️',
-                      bootstyle=('info', 'outline'), command=playlast)
+                     bootstyle=('primary', 'outline'), command=playlast)
 time_Label = Label(buttons_Frame, textvariable=time_str, font=('微软雅黑', 10))
-Scale(bootstyle="info")
+Scale(bootstyle="primary")
 
 artist_Label.pack(side='left', fill='x', padx=5, pady=5)
 Label(info_Frame, text='•', font=('微软雅黑', 25)).pack(side='left')
 album_Label.pack(side='left', fill='x', padx=5, pady=5)
-last_Button.pack(side='left', fill='x', padx=10, pady=5)
+last_Button.pack(side='left', fill='x', padx=20, pady=5)
 pause_Button.pack(side='left', fill='x', padx=5, pady=5)
-next_Button.pack(side='left', fill='x', padx=10, pady=5)
+next_Button.pack(side='left', fill='x', padx=20, pady=5)
 ask_file_Button.pack(side='left', fill='x', padx=5, pady=5)
+setting_Button.pack(side='left', fill='x', padx=5, pady=5)
 time_Label.pack(side='left', anchor='e', expand='yes', padx=5)
-
 buttons_Frame.pack(side='bottom', fill='both')
 pos_Scale.pack(side='bottom', fill='x')
-
 img_Label.pack(side='left', pady=10, padx=20)
 title_Label.pack(pady=5)
 info_Frame.pack()
+
+
 
 pos_Scale.bind("<Button-1>", lambda a: click())
 pos_Scale.bind("<ButtonRelease-1>", lambda a: release())
@@ -220,4 +235,10 @@ img_Label.bind("<Double-Button-1>", lambda a: startfile('song.png'))
 img_Label.bind("<Button-3>", lambda a: startfile('song.png'))
 window_update()
 
+f = open('song.png','w')
+f.close()
+with open('settings.json','r') as f:
+    settings = eval(f.read())
+
+style.theme_use(settings["theme"])
 window.mainloop()
